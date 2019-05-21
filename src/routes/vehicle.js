67e567227f81dfd8b67082670fad74258fda72ac
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-05-21 12:51:15 +0100

    create route to retrieve all available cars in the app [finish #166152124)]

--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -4,5 +4,6 @@ import carController from '../controllers/carController';
 const router = express.Router();
 
 router.post('/car', carController.createCar);
+router.get('/car', carController.getAllCars);
 
 export default router;
