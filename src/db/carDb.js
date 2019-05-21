bda07981b6a19886f49b2ab623c10b26e64d1666
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-05-21 14:54:13 +0100

    Create get a single car route[finish #166153063]

--- src/db/carDb.js
+++ src/db/carDb.js
@@ -13,7 +13,7 @@ const vehicles = [
   },
 
   {
-    id: 1,
+    id: 2,
     userId: 1,
     createdOn: new Date(),
     state: 'new',
@@ -25,7 +25,7 @@ const vehicles = [
   },
 
   {
-    id: 1,
+    id: 3,
     userId: 1,
     createdOn: new Date(),
     state: 'new',
