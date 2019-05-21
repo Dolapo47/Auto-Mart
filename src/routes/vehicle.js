4d4f952fcdebaf247ebf685d7eb2e7a9bd056c57
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-05-21 20:53:03 +0100

    Added file to route file

--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -7,6 +7,7 @@ router.post('/car', carController.createCar);
 router.get('/car', carController.getAllCars);
 router.get('/car/:car_id', carController.getOneCar);
 router.patch('/car/:car_id', carController.updateStatus);
+router.patch('/car/:car_id', carController.updatePrice);
 router.delete('/car/:car_id', carController.deleteCar);
 
 export default router;
