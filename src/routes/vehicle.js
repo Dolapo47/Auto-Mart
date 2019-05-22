9b7b9f0fea0042ee18221e9ac3f1728d29865e3f
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-05-22 10:53:38 +0100

    made corrections to the route

--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -6,8 +6,8 @@ const router = express.Router();
 router.get('/car', carController.getAllCars);
 router.get('/car/:car_id', carController.getOneCar);
 router.post('/car', carController.createCar);
-router.patch('/car/:car_id', carController.updateStatus);
-router.patch('/car/:car_id', carController.updatePrice);
+router.patch('/car/:car_id/status', carController.updateStatus);
+router.patch('/car/:car_id/price', carController.updatePrice);
 router.delete('/car/:car_id', carController.deleteCar);
 router.get('/car?status=availabl', carController.getAvailableCars);
 
