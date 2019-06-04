962bb8e96119d21e6d71dc7aff97e4ffa45aabe8
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-04 15:54:04 +0100

    create test cases for update status and when no car matched the get condition

--- test/car.spec.js
+++ test/car.spec.js
@@ -53,10 +53,39 @@ describe('Car', () => {
   it('should update the price of vehicle in the app', (done) => {
     chai.request(app)
       .patch('/api/v1/car/1/price')
+      .send({ price: 2000000 })
       .end((err, res) => {
         const { body } = res;
         if (err) done(err);
         expect(body.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('should update the status of vehicle in app', (done) => {
+    chai.request(app)
+      .patch('/api/v1/car/1/status')
+      .send({ status: 'sold' })
+      .end((err, res) => {
+        const { body } = res;
+        if (err) done(err);
+        expect(body.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('it should return an error if no vehicle is found', (done) => {
+    chai.request(app)
+      .get('/api/v1/car/5')
+      .send({})
+      .end((err, res) => {
+        const { body } = res;
+        if (err) done(err);
+        expect(body.status).to.equal(404);
+        expect(res.body.message).to.be.equal('No vehicle matched the specified criteria');
+        expect(res.body.message).to.be.an('string');
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.be.a('number');
         done();
       });
   });
