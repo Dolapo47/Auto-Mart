c80c493640ccd12251c06a59bd9354c3a2cfc69d
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-07 09:34:23 +0100

    Create order feature and tests

--- src/helper/validations/validateOrderInput.js
+++ src/helper/validations/validateOrderInput.js
@@ -1,17 +1,42 @@
-/* eslint-disable require-jsdoc */
-import joi from '@hapi/joi';
+import Validator from 'validator';
+import isEmpty from '../isEmpty';
 
-class validate {
-  static validateOrder(order) {
-    const schema = joi.object().keys({
-      amount_offered: joi.number()
-        .min(1)
-        .max(13)
-        .required()
-        .error('Amount must be between 1 and 13 digits'),
-    });
-    return joi.validate(order, schema);
+const validateOrderInput = (data) => {
+  const errors = {};
+  data.amount_offered = isEmpty(data.amount_offered) === true ? '' : data.amount_offered;
+  data.userId = isEmpty(data.userId) === true ? '' : data.userId;
+  data.carId = isEmpty(data.carId) === true ? '' : data.carId;
+  if (!Validator.isFloat(data.amount_offered)) {
+    errors.amount_offered = 'Number must be in 0.00 format';
   }
-}
 
-export default validate;
+  if (Validator.isEmpty(data.amount_offered)
+    || !Validator.isLength(data.amount_offered, { min: 1, max: 13 })) {
+    errors.amount_offered = 'Last name must be between 1 and 13 characters';
+  }
+
+  if (!Validator.isNumeric(data.userId)) {
+    errors.userId = 'User id should be a number';
+  }
+
+  if (Validator.isEmpty(data.userId)
+    || !Validator.isLength(data.userId, { min: 1, max: 3 })) {
+    errors.userId = 'Last name must be between 1 and 3 characters';
+  }
+
+  if (!Validator.isNumeric(data.carId)) {
+    errors.carId = 'Car id should be a number';
+  }
+
+  if (Validator.isEmpty(data.carId)
+    || !Validator.isLength(data.carId, { min: 1, max: 3 })) {
+    errors.carId = 'Last name must be between 1 and 3 characters';
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
+
+
+export default validateOrderInput;
--- src/routes/order.js
+++ src/routes/order.js
@@ -4,7 +4,7 @@ import orderController from '../controllers/orderController';
 const router = express.Router();
 
 router.post('/order', orderController.createOrder);
-router.patch('/order/:orderId/price', orderController.updateOrder);
+// router.patch('/order/:orderId/price', orderController.updateOrder);
 
 
 export default router;
