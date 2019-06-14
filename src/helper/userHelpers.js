dc0584ac6f4447c319d29a42a7f106f8b0617ddc
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-14 20:57:37 +0100

    create helper to verify token

--- src/helper/userHelpers.js
+++ src/helper/userHelpers.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { errorMessage } from './validations/responseMessages';
+import { responseMessage } from './validations/responseMessages';
 
 
 dotenv.config();
@@ -10,4 +10,20 @@ const secretKey = process.env.SECRET;
 export const generateToken = (email, userId) => {
   const token = jwt.sign({ email, userId }, secretKey, { expiresIn: 84600 });
   return token;
+};
+
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return responseMessage(res, 401, 'Authorization token was not provided');
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.body.userId = decoded.userId;
+    req.user = decoded;
+    return next();
+  } catch (err) {
+    return responseMessage(res, 401, 'Invalid authorization token');
+  }
 };
