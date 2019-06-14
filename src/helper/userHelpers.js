cbf508670fc469bd75270dace9e39eadfa910358
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-14 01:41:42 +0100

    refactor code in userHelper file

--- src/helper/userHelpers.js
+++ src/helper/userHelpers.js
@@ -6,23 +6,8 @@ import { errorMessage } from './validations/responseMessages';
 dotenv.config();
 const secretKey = process.env.SECRET;
 
+// eslint-disable-next-line import/prefer-default-export
 export const generateToken = (email, userId) => {
   const token = jwt.sign({ email, userId }, secretKey, { expiresIn: 84600 });
   return token;
-};
-
-export const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) {
-    return errorMessage(res, 401, 'Authorization token was not provided');
-  }
-
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.body.userId = decoded.userId;
-    req.user = decoded;
-    return next();
-  } catch (err) {
-    return errorMessage(res, 401, 'Invalid authorization token');
-  }
 };
