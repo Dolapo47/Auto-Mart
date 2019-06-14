5b29ca04ee393d6ea4346bafec4c52faad4bd2ee
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-14 19:34:02 +0100

    refactored code in the car route and user route

--- src/helper/validations/responseMessages.js
+++ src/helper/validations/responseMessages.js
@@ -1,4 +1,4 @@
-export const errorMessage = (res, statusCode, message) => {
+export const responseMessage = (res, statusCode, message) => {
   res.status(statusCode).json({
     status: statusCode,
     error: message,
@@ -10,13 +10,6 @@ export const userMessage = (res, statusCode, message, token, user) => {
     status: statusCode,
     success: message,
     data: [{ token, user, }]
-  });
-};
-
-export const carMessage = (res, statusCode, message) => {
-  res.status(statusCode).json({
-    status: statusCode,
-    success: message,
   });
 };
 
