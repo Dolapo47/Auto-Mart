62cef2428075cc3fa1e15327c7def3cb05df777e
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-15 12:32:08 +0100

    create protection for routes and refactor test file

--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import carController from '../controllers/carController';
+import { verifyToken } from '../helper/userHelpers';
 
 const router = express.Router();
 
-router.get('/car', carController.getAllCars);
-router.get('/car/:car_id', carController.getOneCar);
-router.post('/car', carController.createCar);
-router.patch('/car/:car_id/status', carController.updateStatus);
-router.patch('/car/:car_id/price', carController.updatePrice);
-router.delete('/car/:car_id', carController.deleteCar);
+router.get('/car', verifyToken, carController.getAllCars);
+router.get('/car/:car_id', verifyToken, carController.getOneCar);
+router.post('/car', verifyToken, carController.createCar);
+router.patch('/car/:car_id/status', verifyToken, carController.updateStatus);
+router.patch('/car/:car_id/price', verifyToken, carController.updatePrice);
+router.delete('/car/:car_id', verifyToken, carController.deleteCar);
 
 
 export default router;
