3b3f3298cef4042aa5940bd32e185b326802cb7e
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-17 06:51:42 +0100

    write test for car routes

--- src/routes/vehicle.js
+++ src/routes/vehicle.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import carController from '../controllers/carController';
 import { verifyToken } from '../helper/userHelpers';
+import { validatePostCar } from '../helper/validations/validateCarInput';
 
 const router = express.Router();
 
 router.get('/car', verifyToken, carController.getAllCars);
 router.get('/car/:car_id', verifyToken, carController.getOneCar);
-router.post('/car', verifyToken, carController.createCar);
-router.patch('/car/:car_id/status', verifyToken, carController.updateStatus);
-router.patch('/car/:car_id/price', verifyToken, carController.updatePrice);
+router.post('/car', verifyToken, validatePostCar, carController.createCar);
+router.patch('/car/:car_id/status', carController.updateStatus);
+router.patch('/car/:car_id/price', carController.updatePrice);
 router.delete('/car/:car_id', verifyToken, carController.deleteCar);
 
 
