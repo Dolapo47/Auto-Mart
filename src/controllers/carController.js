29ebc5834884bbc22153d5e2807227e9cff14ff8
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-17 06:51:42 +0100

    write test for car routes

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,15 +1,11 @@
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable require-jsdoc */
 import vehicles from '../db/carDb';
-import validateCarInput from '../helper/validations/validateCarInput';
 import { responseMessage, retrieveCarMessage } from '../helper/validations/responseMessages';
 import carQueries from '../helper/carHelpers';
 
 class carController {
   static createCar(req, res) {
-    // const { errors, isValid } = validateCarInput(req.body);
-    // if (!isValid) {
-    //   return res.status(400).json({ errors });
-    // }
     const vehicle = {
       id: vehicles.length + 1,
       userId: 3,
@@ -36,32 +32,24 @@ class carController {
     return retrieveCarMessage(res, 200, 'Vehicles successfully retrieved', item);
   }
 
-  // static getAvailableCars(req, res) {
-  //   const { status } = req.query;
-  //   const isInvalidStatus = (status !== 'available');
-  //   if (isInvalidStatus) return errorMessage(res, 403, 'you do not have access to this resource');
-  //   const availableCars = vehicles.find(car => car.status === 'available');
-  //   if (!availableCars) return errorMessage(res, 404, 'No vehicle matched the specified criteria');
-  //   return retrieveCarMessage(res, 200, 'Vehicles successfully retrieved', availableCars);
-  // }
-
   static updateStatus(req, res) {
     const id = parseInt(req.params.car_id, 10);
     const userId = parseInt(req.body.userId, 10);
-    const item = carQueries.findCar(id, userId);
-    item.status = req.body.status;
+    const { status } = req.body;
+    const item = carQueries.findOneCar(id, userId);
     if (!item) return responseMessage(res, 404, 'No vehicle matched the specified criteria');
-    return responseMessage(res, 200, 'Vehicle successfully updated');
+    item.status = status;
+    return retrieveCarMessage(res, 200, 'Vehicle successfully updated', item);
   }
 
   static updatePrice(req, res) {
     const id = parseInt(req.params.car_id, 10);
     const userId = parseInt(req.body.userId, 10);
+    const { price } = req.body;
     const item = carQueries.findOneCar(id, userId);
-    item.price = req.body.price;
-    console.log(item);
     if (!item) return responseMessage(res, 404, 'No vehicle matched the specified criteria');
-    return responseMessage(res, 200, 'Vehicle successfully updated');
+    item.status = price;
+    return retrieveCarMessage(res, 200, 'Vehicle successfully updated', item);
   }
 
   static deleteCar(req, res) {
---
+++ src/helper/validations/validateCarInput.js
@@ -0,0 +1,105 @@
+import {
+  isNotSpecified,
+  pureNumber,
+  money,
+  pureString
+} from './validator.schema';
+
+import { responseMessage } from './responseMessages';
+
+
+// eslint-disable-next-line import/prefer-default-export
+export const validatePostCar = [
+  /**
+     * VALIDATION ORDER:
+     *  state
+     *  price
+     *  manufacturer
+     *  model
+     *  bodyType
+     *  image
+     */
+
+  // STATE VALIDATION: ensure state is either new or used
+  (req, res, next) => {
+    const { state } = req.body;
+    if (!state) {
+      return responseMessage(res, 422,
+        'please specify the state of the automobile (new/used)');
+    }
+    const lowerCase = state.toLowerCase();
+    return (lowerCase === 'new' || lowerCase === 'used')
+      ? next()
+      : responseMessage(res, 422, 'car state can either be "new" or "used"');
+  },
+
+  // PRICE VALIDATION
+  (req, res, next) => {
+    const { price } = req.body;
+    const message = isNotSpecified(price, 'price')
+            || money.doValidation(price, 'price', 12);
+    return message
+      ? responseMessage(res, 422, message)
+      : next();
+  },
+
+  // MANUFACTURER VALIDATION
+  (req, res, next) => {
+    const { manufacturer } = req.body;
+    const message = isNotSpecified(manufacturer, 'manufacturer')
+            || pureString.isInvalid(manufacturer, 'manufacturer')
+            || pureString.isTooLong(manufacturer, 'manufacturer', 30);
+
+    return message
+      ? responseMessage(res, 422, message)
+      : next();
+  },
+
+  // MODEL VALIDATION
+  (req, res, next) => {
+    const { model } = req.body;
+    const message = isNotSpecified(model, 'model')
+            || pureString.isNotAlphaNumeric(model, 'model')
+            || pureString.isTooLong(model, 'model', 30);
+
+    return message
+      ? responseMessage(res, 422, message)
+      : next();
+  },
+
+  // BODY TYPE VALIDATION
+  (req, res, next) => {
+    const { bodyType } = req.body;
+    const message = isNotSpecified(bodyType, 'body type')
+            || pureString.isInvalid(bodyType, 'body type')
+            || pureString.isTooLong(bodyType, 'body type', 25);
+
+    return message
+      ? responseMessage(res, 422, message)
+      : next();
+  },
+];
+
+export const validateIdParam = (req, res, next) => {
+  const { carId } = req.params;
+  const message = isNotSpecified(carId, 'car id')
+      || pureNumber.isInvalid(carId, 'car id');
+  req.params.carId = +carId;
+  return message
+    ? responseMessage(res, 422, message)
+    : next();
+};
+
+
+export const validatePatchPrice = [
+  validateIdParam,
+  // PRICE VALIDATION
+  (req, res, next) => {
+    const { newPrice } = req.body;
+    const message = isNotSpecified(newPrice, 'new price')
+          || money.doValidation(newPrice, 'new price', 12);
+    return message
+      ? responseMessage(res, 422, message)
+      : next();
+  }
+];
