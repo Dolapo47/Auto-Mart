bbc528cecd5f22ec587bb07ed573d47808bc0005
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-20 10:02:47 +0100

    create new car ad route

--- src/helper/multer.js
+++ src/helper/multer.js
@@ -1,13 +1,14 @@
 import multer from 'multer';
 
-const multerUpload = multer({
-  storage: multer.diskStorage(),
-  fileFilter: (req, file, cb) => {
-    if (!file.mimetype.match(/jpg|jpeg|png|gif$i/)) {
-      cb(new Error('File is not supported'), false);
-    }
-    cb(null, true);
-  }
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
 });
 
-export default multerUpload;
+const upload = multer({ storage });
+
+export default upload;
