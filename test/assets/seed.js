3367dac4a98c701f792e464fb0241cfa21269901
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-24 16:20:01 +0100

    write test for user routes

---
+++ test/assets/seed.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+import '@babel/polyfill';
+import pool from '../../src/db/index';
+import createQuery from '../../src/db/createTables';
+import dropQuery from '../../src/db/dropTables';
+
+
+const createTables = async () => {
+  const password = bcrypt.hashSync('biggie', 10);
+  const userPass = bcrypt.hashSync('1234jaycee', 10);
+  await pool.query(`${dropQuery}; ${createQuery};`);
+
+  const newUser = await pool.query('INSERT INTO users (firstname, lastname, email, password, address, is_admin) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;', ['Chinonso', 'Amadi', 'amadi@gmail.com', password, '239, Epic Towers, ikorodu road', 't']);
+  const newUser2 = await pool.query('INSERT INTO users (firstname, lastname, email, password, address, is_admin) VALUES($1, $2, $3, $4, $5, $6) RETURNING *;', ['nonso', 'Amadi', 'amadi@aol.com', userPass, '239, Epic Towers, ikorodu road', 'f']);
+  const car = await pool.query('INSERT INTO cars(ownerId, ownerEmail, createdon, state, status, price, manufacturer, model, body_type, image_url, flagged) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9, $10, $11) RETURNING * ;', [newUser.rows[0].id, newUser.rows[0].email, new Date(), 'new', 'available', 200000.089, 'bmq', 'ferrai', 'truck', 'https://classcar.com/car?=toyota', false]);
+  const car2 = await pool.query('INSERT INTO cars(ownerId, ownerEmail, createdon, state, status, price, manufacturer, model, body_type, image_url, flagged) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9, $10, $11) RETURNING * ;', [2, 'amadi@aol.com', new Date(), 'new', 'available', 200000.089, 'bmq', 'ferrai', 'truck', 'https://classcar.com/car?=toyota', false]);
+
+  const createdOn = new Date().toLocaleDateString();
+
+  const makeOrder = await pool.query('INSERT into orders(car_id, buyer_id, createdon ,amountOffered, status) VALUES($1, $2, $3, $4, $5) RETURNING * ;', [1, 2, createdOn, 30000.00, 'pending']);
+};
+
+export default createTables;
