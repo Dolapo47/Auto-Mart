f756a64a10c03fdff0c59e2d53af4ddb12df1b73
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-24 08:03:59 +0100

    create update route for orders

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -6,55 +6,47 @@ import pool from '../db';
 class orderController {
   static async createOrder(req, res) {
     const { errors, isValid } = validateOrderInput(req.body);
-    if (!isValid) return responseMessage(res, 422, errors);
+    if (!isValid) return responseMessage(res, 400, errors);
     const { id } = req.user;
-    const carId = parseInt(req.body.carId, 10);
-    const { priceOffered } = req.body;
+    const { carId, priceOffered } = req.body;
     const price = Number(priceOffered).toFixed(2);
     const status = 'pending';
     try {
-      const checkOrder = await pool.query('SELECT * FROM orders WHERE (car_id = $1 AND buyer_id = $2) OR status = $3;', [carId, id, 'pending']);
-      if (checkOrder.rowCount > 0) {
-        return responseMessage(res, 409, 'The order already exist');
+      const carExist = await pool.query('SELECT id, price FROM cars WHERE id=$1; ', [carId]);
+      if (carExist.rowCount <= 0) {
+        return res.status(404).send({
+          status: 'error',
+          error: 'Car does not exist',
+        });
       }
       const createdOn = new Date().toLocaleDateString();
+
       const makeOrder = await pool.query('INSERT into orders(car_id, buyer_id, createdon ,amountOffered, status) VALUES($1, $2, $3, $4, $5) RETURNING * ;', [carId, id, createdOn, price, status]);
-      return res.status(201).send({
-        status: 'success',
-        data: {
-          id: makeOrder.rows[0].id,
-          buyerId: makeOrder.rows[0].buyer_id,
-          carId,
-          createdOn: makeOrder.rows[0].createdon,
-          priceOffered: makeOrder.rows[0].amountoffered,
-          status: makeOrder.rows[0].status,
-        },
-      });
+      return retrieveCarMessage(res, 201, 'order created', makeOrder.rows[0]);
+    } catch (error) {
+      return responseMessage(res, 404, error.message);
+    }
+  }
+
+  static async updateOrder(req, res) {
+    const { errors, isValid } = validateOrderUpdate(req.body);
+    if (!isValid) return responseMessage(res, 400, errors);
+    const { orderId } = req.params;
+    const { newOffer } = req.body;
+    const { id } = req.user;
+    try {
+      const checkUserOrder = await pool.query('SELECT * FROM orders WHERE buyer_id=$1 AND id=$2;', [id, orderId]);
+      if (checkUserOrder.rowCount <= 0) {
+        return responseMessage(res, 404, 'order not found');
+      }
+      const updateOrderPrice = await pool.query('UPDATE orders SET amountoffered=$1 WHERE id=$2 RETURNING *;', [newOffer, checkUserOrder.rows[0].id]);
+      return retrieveCarMessage(res, 200, 'success', updateOrderPrice.rows);
     } catch (error) {
       return res.status(400).send({
         status: 'error',
         error: error.message,
       });
     }
-  }
-
-  static updateOrder(req, res) {
-    const { errors, isValid } = validateOrderUpdate(req.body);
-    if (!isValid) return responseMessage(res, 422, errors);
-
-    const id = parseInt(req.params.orderId, 10);
-    const userId = parseInt(req.body.userId, 10);
-    const newAmount = req.body.amountOffered;
-
-    const checkOrder = orders.filter(order => order.userId === userId && order.id === id);
-    if (checkOrder < 1) {
-      return responseMessage(res, 409, 'The order does not exist');
-    }
-    if (checkOrder[0].status === 'pending') {
-      checkOrder[0].amountOffered = newAmount;
-      return retrieveCarMessage(res, 200, 'order successfully updated', checkOrder[0]);
-    }
-    return responseMessage(res, 400, 'The order has been approved');
   }
 }
 
