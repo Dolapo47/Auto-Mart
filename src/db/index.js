613f7899cf47c9787752e1d31309e7bb6268b57e
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-06-25 13:06:05 +0100

    create script to manage database switches

--- src/db/index.js
+++ src/db/index.js
@@ -7,6 +7,6 @@ dotenv.config();
 
 const env = process.env.NODE_ENV;
 
-const pool = env === 'test' ? new Pool({ connectionString: process.env.DATABASE_URL }) : new Pool({ connectionString: process.env.DB });
+const pool = env === 'test' ? new Pool({ connectionString: process.env.DATABASE_URL }) : new Pool({ connectionString: process.env.TEST_URL });
 
 export default pool;
