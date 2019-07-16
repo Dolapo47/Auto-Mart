53e4e0095e4c2642d5cb1d666d08f85d451a514f
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-07-16 23:13:24 +0100

    refactor post car controller

--- src/helper/userHelpers.js
+++ src/helper/userHelpers.js
@@ -8,15 +8,17 @@ const secretKey = process.env.SECRET;
 
 
 export const verifyToken = (req, res, next) => {
-  try {
-    if (!req.headers.authorization) return res.status(401).send({ status: 401, error: 'You must be logged in to use this route' });
-    const token = req.headers.authorization.split(' ')[1];
+  let token = req.header.authorization;
+  if (token) {
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length);
+    }
     const decoded = jwt.verify(token, secretKey);
     req.user = decoded;
     return next();
-  } catch (e) {
-    console.log(e);
-    res.status(401).send({ status: 401, error: 'Auth failed' });
   }
-
+  return res.status(401).json({
+    status: 401,
+    error: 'Please provide a valid token',
+  });
 };
