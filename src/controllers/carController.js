373db5762cecb39e0b5786eb8f1262dda020f8d8
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-07-16 20:15:56 +0100

    refactor post car controller

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -6,8 +6,8 @@ import validate from '../helper/validations/validateInput';
 
 class carController {
   static async createCar(req, res) {
-    const { error } = validate.validateCarInput(req.body);
-    if (error) return errorMessage(res, 422, error.details[0].message);
+    // const { error } = validate.validateCarInput(req.body);
+    // if (error) return errorMessage(res, 422, error.details[0].message);
     const { id, email } = req.user;
     const {
       manufacturer, model, state, price, body_type, image_url,
