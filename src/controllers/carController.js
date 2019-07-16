fade6231f930b41da04b845e999f9acdd96a17b4
dolapo47 <adeleyeadedolapomayowa@gmail.com>
2019-07-16 12:28:21 +0100

    refactor userHelper file

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -12,16 +12,17 @@ class carController {
       console.log(error);
       return errorMessage(res, 422, error.details[0].message);
     }
+    const img = 'http://res.cloudinary.com/dolapo/image/upload/v1563270605/tni2saqewiv5l5ljnsvj.jpg';
     const { id, email } = req.user;
     const {
-      manufacturer, model, state, price, body_type,
+      manufacturer, model, state, price, body_type, image_url
     } = req.body;
     console.log(req.body);
     const Formatted_price = parseFloat(price).toFixed(2);
     const created_on = new Date().toLocaleString();
     const status = 'available';
     try {
-      const newCar = await DB.query('INSERT INTO cars(owner_id, owner_email, created_on, state, status, price, manufacturer, model, body_type, flagged) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9, $10, $11) RETURNING *;', [id, email, created_on, state, status, Formatted_price, manufacturer, model, body_type, false]);
+      const newCar = await DB.query('INSERT INTO cars(owner_id, owner_email, created_on, state, status, price, manufacturer, model, body_type, img_url, flagged) VALUES($1, $2, $3, $4, $5, $6, $7, $8 , $9, $10, $11) RETURNING *;', [id, email, created_on, state, status, Formatted_price, manufacturer, model, body_type, img, false]);
       return retrieveCarMessage(res, 201, 'Vehicle created', newCar.rows[0]);
     } catch (errors) {
       console.log(errors);
--- src/helper/cloudinary.js
+++ src/helper/cloudinary.js
@@ -4,7 +4,7 @@ import cloudinary from './cloudinarySetup';
 const cloudinaryUpload = async (req, res, next) => {
   try {
     await cloudinary.v2.uploader.upload(req.files[0].path, (error, result) => {
-      req.body.image_url = result.url;
+      req.body.image_url = 'http://res.cloudinary.com/dolapo/image/upload/v1563270605/tni2saqewiv5l5ljnsvj.jpg';
     });
   } catch (err) {
     return err;
